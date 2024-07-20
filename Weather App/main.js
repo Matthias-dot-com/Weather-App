4074c89e3e706401c2699358a6e97f3cba55b250
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-07-20 23:13:40 +0400

    API integration done

--- Weather App/main.js
+++ Weather App/main.js
@@ -1,1 +1,13 @@
 import "./style.css";
+import { getWeather } from "./weather";
+
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
+  renderWeather
+).catch(e=>{
+    console.error(e)
+    alert("Error")
+})
+
+function renderWeather(current,daily,hourly){
+
+}
